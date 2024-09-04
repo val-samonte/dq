2906bc8af7b99b8b15e9a08ca16dd490967f3043
Gel <angel.balano@gmail.com>
2024-09-04 08:33:40 +0800

    Add modal menu

--- app/frontend/src/components/HeaderMenu.tsx
+++ app/frontend/src/components/HeaderMenu.tsx
@@ -1,9 +1,14 @@
 import { CaretLeft, List } from "@phosphor-icons/react"
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 
 export function HeaderMenu() {
+  const [showMenu, setShowMenu] = useState(false)
   const location = useLocation()
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
 
   const navLinks = useMemo(() => {
     const common = [
@@ -77,7 +82,7 @@ export function HeaderMenu() {
     ]
   }, [location])
 
-  return (<nav className='flex flex-none px-3 py-2 justify-between items-center'>
+  return (<nav className='flex flex-none px-3 py-2 justify-between items-center relative'>
     <div className='font-serif flex gap-2 items-center'>
       {navLinks.map((item, i) => {
         if (item.type === 'back') {
@@ -90,8 +95,22 @@ export function HeaderMenu() {
         return <span key={'nav_' + i}>{item.value}</span>
       })}
     </div>
-    <button>
+    <button data-collapse-toggle="navbar-hamburger" type="button" className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false" onClick={toggleMenu}>
+      <span className="sr-only">Open main menu</span>
       <List size={32} />
     </button>
+    <div className={`${!showMenu && 'hidden'} w-full absolute inset-x-0 top-10 h-16 z-50`} id="navbar-hamburger">
+      <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+        <li>
+          <a href="/" className="block py-2 px-3 bg-sky-300 rounded dark:bg-sky-300 text-black" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Settings</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white">Connect Wallet</a>
+        </li>
+      </ul>
+    </div>
   </nav>)
 }
