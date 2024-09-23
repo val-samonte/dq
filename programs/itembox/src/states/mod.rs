533d11fc91e18555c0289bffb27759332d5966cf
Val <val.samonte@gmail.com>
2024-09-24 01:54:57 +0800

    Recipe PDA

--- programs/itembox/src/states/mod.rs
+++ programs/itembox/src/states/mod.rs
@@ -1,5 +1,7 @@
 pub mod blueprint;
 pub mod main;
+pub mod recipe;
 
 pub use blueprint::*;
 pub use main::*;
+pub use recipe::*;
---
+++ programs/itembox/src/states/recipe.rs
@@ -0,0 +1,38 @@
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct Recipe {
+
+  /// Bump nonce of the PDA. (1)
+  pub bump: u8,
+
+  /// The blueprint to use to produce the item of this recipe. (32)
+  pub blueprint: Pubkey,
+
+  /// The list of ingredients for this recipe. (4 + dynamic)
+  pub ingredients: Vec<Ingredient>,
+}
+
+impl Recipe {
+  pub fn len(ingredients_count: usize) -> usize {
+    let ingredient_size = 32 + 1 + 8 + 1;
+
+    8 + 1 + 32 + 4 + (ingredient_size * ingredients_count)
+  }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct Ingredient {
+
+    /// A blueprint or the mint address of the SPL token. (32)
+    pub asset: Pubkey,
+
+    /// SPL [0], Blueprint [1]. (1)
+    pub asset_type: u8,
+
+    /// Amount needed, only applicable to fungible assets. (8)
+    pub amount: u64,
+
+    /// What to do with this asset after crafting the recipe. Burn [0], Transfer [1]. (1)
+    pub consume_method: u8,
+}
