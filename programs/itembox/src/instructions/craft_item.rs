9631635708466707893019603728bd12cc054b87
Val <val.samonte@gmail.com>
2024-09-26 11:10:03 +0800

    Not sure if this will even work lol

--- programs/itembox/src/instructions/craft_item.rs
+++ programs/itembox/src/instructions/craft_item.rs
@@ -1,9 +1,13 @@
 use std::{collections::HashMap, str::FromStr};
 
 use anchor_lang::prelude::*;
-use anchor_spl::{associated_token, token::{self, spl_token, Burn}, token_2022::{self, spl_token_2022, Burn as Burn2022}};
+use anchor_spl::{token::{self, spl_token, Burn, Transfer}, token_2022::{self, spl_token_2022, Burn as Burn2022, TransferChecked}};
 use solana_program::program_pack::Pack;
+use spl_token::state::Mint;
 use spl_token::state::Account as TokenAccount;
+use anchor_spl::token::Token;
+use anchor_spl::token_interface::Token2022;
+use anchor_spl::associated_token::AssociatedToken;
 
 use crate::states::{Blueprint, Recipe};
 
@@ -26,19 +30,22 @@ pub struct CraftItem<'info> {
   #[account(mut)]
   pub owner: Signer<'info>,
 
-  pub token_program: Program<'info, token::Token>,
-  pub token_program_2022: Program<'info, token_2022::Token2022>,
-  pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+  pub token_program: Program<'info, Token>,
+  pub token_program_2022: Program<'info, Token2022>,
+  pub associated_token_program: Program<'info, AssociatedToken>,
   pub rent: Sysvar<'info, Rent>,
   pub system_program: Program<'info, System>,
 }
 
-pub fn craft_item_handler(ctx: Context<CraftItem>, _args: CreateItemArgs) -> Result<()> {
+pub fn craft_item_handler<'a, 'b, 'c, 'info>(
+  ctx: Context<'a, 'b, 'c, 'info, CraftItem<'info>>,
+  _args: CreateItemArgs
+) -> Result<()> {
 
   let recipe = &ctx.accounts.recipe;
   let owner_pubkey = ctx.accounts.owner.key();
 
-  let mut account_map: HashMap<Pubkey, &AccountInfo> = HashMap::new();
+  let mut account_map: HashMap<Pubkey, &AccountInfo<'info>> = HashMap::new();
   for account in ctx.remaining_accounts.iter() {
     account_map.insert(*account.key, account);
   }
@@ -51,7 +58,6 @@ pub fn craft_item_handler(ctx: Context<CraftItem>, _args: CreateItemArgs) -> Res
           // if let Ok(blueprint_account) = Blueprint::try_from_slice(&account_info.data.borrow()) {
             // let mint = account_map.get(&blueprint_account.mint).unwrap();
           // }
-
         }
         _ => {
           let token_program_id = if ingredient.asset_type == 1 {
@@ -72,31 +78,84 @@ pub fn craft_item_handler(ctx: Context<CraftItem>, _args: CreateItemArgs) -> Res
               return Err(CraftItemError::InsufficientIngredientAmount.into());
             }
 
-            // ata_account_info.to_account_info()
-
             match ingredient.consume_method {
+              // burn
               1 => {
-                // Burn {
-                //   mint: asset_account.to_account_info(),
-                //   from: ata_account_info.to_account_info(),
-                //   authority: ctx.accounts.owner.to_account_info(),
-
-                // };
-                // burn
-                // token::burn(
-                //   CpiContext::new(
-                //       ctx.accounts.token_program.to_account_info(),
-                //       Burn {
-                //           mint: mint_account.to_account_info(),
-                //           from: ata_account.to_account_info(),
-                //           authority: ctx.accounts.owner.to_account_info(),
-                //       },
-                //   ),
-                //   amount,
-                // )
+                if ingredient.asset_type == 1 {
+                  token::burn(
+                    CpiContext::new(
+                      ctx.accounts.token_program.to_account_info(), 
+                      Burn {
+                        mint: asset_account.to_account_info(),    
+                        from: ata_account_info.to_account_info(), 
+                        authority: ctx.accounts.owner.to_account_info(), 
+                      },
+                    ),
+                    ingredient.amount
+                  )?;
+                } else {
+                  token_2022::burn(
+                    CpiContext::new(
+                      ctx.accounts.token_program_2022.to_account_info(),
+                      Burn2022 {
+                        mint: asset_account.to_account_info(),
+                        from: ata_account_info.to_account_info(),
+                        authority: ctx.accounts.owner.to_account_info(),
+                      },
+                    ),
+                    ingredient.amount
+                  )?;
+                }
               }
               2 => {
                 // transfer
+                // get the receiver's ata, the blueprint treasury is the receiver
+
+                let mint_account = deserialize_mint(asset_account)?;
+
+                let receiver_ata_pubkey = get_associated_token_address(
+                  &ctx.accounts.blueprint.treasury,
+                  &spl_token::id(),
+                  &ctx.accounts.blueprint.mint
+                );
+
+                if let Some(receiver_ata_account_info) = account_map.get(&receiver_ata_pubkey) {
+                  let receiver_ata_account = deserialize_ata(receiver_ata_account_info)?;
+                  if receiver_ata_account.amount < ingredient.amount {
+                    return Err(CraftItemError::InsufficientIngredientAmount.into());
+                  }
+
+                  if ingredient.asset_type == 1 {
+                    token::transfer(
+                      CpiContext::new(
+                        ctx.accounts.token_program.to_account_info(),
+                        Transfer {
+                          from: ata_account_info.to_account_info(),
+                          to: receiver_ata_account_info.to_account_info(),
+                          authority: ctx.accounts.owner.to_account_info(),
+                        }
+                      ),
+                      ingredient.amount
+                    )?;
+                  } else {
+                    token_2022::transfer_checked(
+                      CpiContext::new(
+                        ctx.accounts.token_program_2022.to_account_info(),
+                        TransferChecked {
+                          mint: asset_account.to_account_info(),
+                          from: ata_account_info.to_account_info(),
+                          to: receiver_ata_account_info.to_account_info(),
+                          authority: ctx.accounts.owner.to_account_info(),
+                        }
+                      ),
+                      ingredient.amount,
+                      mint_account.decimals
+                    )?;
+                  }
+
+                } else {
+                  return Err(CraftItemError::MissingIngredientAccount.into());
+                }
               }
               _ => {
                 // retain
@@ -112,7 +171,7 @@ pub fn craft_item_handler(ctx: Context<CraftItem>, _args: CreateItemArgs) -> Res
     }
   }
 
-  // todo: if everything is ok, mint the item
+  // todo: if everything is ok, mint the item as Edition
   // let _blueprint = &ctx.accounts.blueprint;
 
   Ok(())
@@ -150,4 +209,12 @@ pub fn deserialize_ata(account_info: &AccountInfo) -> Result<TokenAccount> {
       Ok(token_account) => Ok(token_account),
       Err(_) => Err(ProgramError::InvalidAccountData.into()),
   }
+}
+
+pub fn deserialize_mint(account_info: &AccountInfo) -> Result<Mint> {
+  // Borrow the data from the AccountInfo and attempt to deserialize it
+  let account_data = account_info.try_borrow_data()?;
+
+  // Use the `unpack` method from SPL Token's Mint struct to deserialize the data
+  Mint::unpack(&account_data).map_err(|_| ProgramError::InvalidAccountData.into())
 }
