e7ee91ff555da72a3180d971e7c1b1ba98f1ee77
Gel <angel.balano@gmail.com>
2024-10-04 04:54:46 +0800

    Add sidebar

---
+++ app/isekai/src/components/Nav.tsx
@@ -0,0 +1,94 @@
+import { FilePlus, HouseLine, Wallet } from '@phosphor-icons/react'
+import { useWalletModal } from '@solana/wallet-adapter-react-ui'
+import cn from 'classnames'
+import { useUserWallet } from '../atoms/userWalletAtom'
+import { trimAddress } from '../utils/trimAddress'
+import { Link } from 'react-router-dom'
+
+export function Nav() {
+  const wallet = useUserWallet()
+  const { setVisible } = useWalletModal()
+
+  return (
+    <div className={cn('bg-gray-700 w-full h-16 sticky top-0 z-10 flex-none')}>
+      <div
+        className={cn(
+          'px-2 gap-2 md:px-5',
+          'w-full h-full max-w-7xl mx-auto flex items-center justify-between'
+        )}
+      >
+        <div className='flex-auto flex items-center gap-2'>
+          <Link to={'/'}>
+            <img
+              src='/logo.png'
+              alt='Itembox'
+              className='w-16 h-16 object-contain aspect-square'
+            />
+          </Link>
+          <input
+            className={cn(
+              'hidden sm:flex',
+              'items-center gap-3',
+              'rounded px-6 py-3 text-lg',
+              'bg-black/20 w-full'
+            )}
+            type='text'
+            placeholder='Search by address, author or Blueprints'
+          />
+        </div>
+        <div className='flex-none'>
+          <Link
+            to='/blueprints/link'
+            className={cn(
+              'w-fit',
+              'flex items-center gap-3',
+              'rounded pr-4 pl-4 py-3 text-lg',
+              'bg-gray-600/50'
+            )}
+          >
+            <FilePlus size={28} />
+          </Link>
+        </div>
+        <div className='flex-none'>
+          <Link
+            to={`/`}
+            className={cn(
+              'w-fit',
+              'flex items-center gap-3',
+              'rounded pr-4 pl-4 py-3 text-lg',
+              'bg-gray-600/50'
+            )}
+          >
+            <HouseLine size={28} />
+          </Link>
+        </div>
+        <div className='flex-none'>
+          <button
+            onClick={() => {
+              if (!wallet?.publicKey) {
+                setVisible(true)
+              } else {
+                wallet.disconnect()
+              }
+            }}
+            className={cn(
+              'w-fit',
+              'flex items-center gap-3',
+              'rounded pr-4 md:pr-6 pl-4 py-3 text-lg',
+              'bg-gray-600/50'
+            )}
+          >
+            <Wallet size={28} />
+            {wallet?.publicKey ? (
+              <span className='text-sm'>
+                {trimAddress(wallet.publicKey.toBase58())}
+              </span>
+            ) : (
+              <span className='hidden md:inline'>Connect Wallet</span>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ app/isekai/src/components/SideBar.tsx
@@ -0,0 +1,27 @@
+import { CardsThree, CirclesThreePlus } from '@phosphor-icons/react'
+
+export function SideBar() {
+  return (
+<div className="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 lg:translate-x-0 lg:static lg:inset-0 h-full">
+    <div className="flex items-center justify-center mt-8">
+        <div className="flex items-center">
+            <span className="mx-2 text-2xl font-semibold text-white">Isekai Dashboard</span>
+        </div>
+    </div>
+
+    <nav className="mt-10">
+        <a className="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100" href="/ui-elements">
+            <CirclesThreePlus size={32} />
+
+            <span className="mx-3">Stories</span>
+        </a>
+
+        <a className="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100" href="/tables">
+            <CardsThree size={32} />
+
+            <span className="mx-3">Import Blueprints</span>
+        </a>
+    </nav>
+</div>
+  )
+}
